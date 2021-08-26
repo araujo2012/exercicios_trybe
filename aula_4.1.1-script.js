32ecfe9544dd418030863e0edeb406e1314a725c
lucas <araujo2012@hotmail.com>
2021-08-26 15:11:41 -0300

    inicia repositorio - inclui exercicio 4.1.1

---
+++ aula_4.1.1-script.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+console.log('adicao')
+console.log(a+b)
+console.log('subtracao')
+console.log(a-b)
+console.log('multiplicacao')
+console.log(a*b)
+console.log('divisao')
+console.log(a/b)
+console.log('modulo')
+console.log(a%b)
