c7046dcd9cbbcff4e50eb166a6fc222e99301849
lucas <araujo2012@hotmail.com>
2021-08-26 15:18:30 -0300

    exercicio 4.1.2

---
+++ aula_4.1.2-script.js
@@ -0,0 +1,9 @@
+let a = 2;
+let b = 10;
+if (a > b){
+    console.log('A primeira variavel e maior.')
+}else if (a < b){
+    console.log('A segunda variavel e maior.')
+}else{
+    console.log('As variaveis sao iguais.')
+}
