5f70ce48ee80537314c39d878d28e42602914625
lucas <araujo2012@hotmail.com>
2021-08-26 21:38:54 -0300

    Concerto de pequeno erro

--- aula_4.2/aula_4.2.bonus-script.js
+++ aula_4.2/aula_4.2.bonus-script.js
@@ -36,5 +36,5 @@ console.log(cnumbers);
 /* Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo: */
 for(let i=0;i<numbers.length;i+=1){
     mnumbers.push((numbers[i+1]||2)*numbers[i]);
-    console.log(mnumbers);
 }
+console.log(mnumbers);
